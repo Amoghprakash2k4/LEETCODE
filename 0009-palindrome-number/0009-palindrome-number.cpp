0827bdb144c200604ee826c65f4ce8bc9325e8e0
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-05-29 23:33:50 +0530

    Time: 4 ms (34.28%), Space: 8.4 MB (92.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,35 +1,35 @@
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        if (x < 0) return false;
-        int original =x;
-       long long rev =0;
-        while(x!=0){
-            int digit= x%10;
-            rev =  rev *10+digit;
-            x/=10;
-        }
-
-        return rev==original;
-    }
-    
-};
-
 // class Solution {
 // public:
 //     bool isPalindrome(int x) {
-//         // Negative numbers are not palindromes
 //         if (x < 0) return false;
-
-//         int original = x;
-//         long long rev = 0;
-
-//         while (x != 0) {
-//             int digit = x % 10;
-//             rev = rev * 10 + digit;
-//             x /= 10;
+//         int original =x;
+//         long long rev =0;
+//         while(x!=0){
+//             int digit= x%10;
+//             rev =  rev *10+digit;
+//             x/=10;
 //         }
 
-//         return rev == original;
+//         return rev==original;
 //     }
+    
 // };
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending in 0 (but not 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedHalf = 0;
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x /= 10;
+        }
+
+        // For even digit count: x == reversedHalf
+        // For odd digit count: x == reversedHalf / 10
+        return (x == reversedHalf) || (x == reversedHalf / 10);
+    }
+};
