a512d848176e12eb7e7e0e1c525cf63003ffcab5
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-05 22:45:41 +0530

    Time: 0 ms (100%), Space: 20.8 MB (27.61%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,12 +1,24 @@
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         for (int i = 0; i < nums.size(); i += 2) {
+//             if (i == nums.size() - 1 || nums[i] != nums[i + 1])
+//                         return nums[i];
+//         }
+
+//         return 0;
+//     }
+// };
+
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i += 2) {
-    if (i == nums.size() - 1 || nums[i] != nums[i + 1])
-        return nums[i];
-}
-
-        return 0;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
     }
 };
