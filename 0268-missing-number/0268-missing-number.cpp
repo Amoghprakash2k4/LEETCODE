db52c88171d15cf0eee6ba0e7de903665b4c6b06
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-05 20:06:59 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.51%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,25 @@
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] != i) {
+//                 return i;
+//             }
+//         }
+//         return nums.size(); // If all numbers 0 to n-1 are present, return n
+//     }
+// };
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != i) {
-                return i;
-            }
+        int n = nums.size();
+        int total = n * (n + 1) / 2;  // Expected sum of 0 to n
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
         }
-        return nums.size(); // If all numbers 0 to n-1 are present, return n
+        return total - sum;  // The missing number
     }
 };
