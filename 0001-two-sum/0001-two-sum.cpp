5ed9dd1c88daaedc4a0cb4ac604b227aee2dbaee
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-07 23:14:44 +0530

    Time: 2 ms (76.64%), Space: 14.9 MB (37.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,13 +4,23 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size() - 1; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};  // Return as a vector
-                }
+        // for (int i = 0; i < nums.size() - 1; i++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             return {i, j};  // Return as a vector
+        //         }
+        //     }
+        // }
+        // return {};  // Return empty vector if no solution found
+
+        unordered_map<int,int> list;
+        for(int i=0; i<nums.size(); i++){
+            int complinment= target-nums[i];
+            if(list.find(complinment) != list.end()){
+                return {list[complinment], i};
             }
+            list[nums[i]] = i;
         }
-        return {};  // Return empty vector if no solution found
+        return {};
     }
 };
