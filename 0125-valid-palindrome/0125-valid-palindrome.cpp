98e50af716b93b2db593bfeb24ecb416a0cb0e22
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-08 22:39:32 +0530

    Time: 1 ms (46.81%), Space: 10.2 MB (28.39%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string valid="";
+        for(char num: s){
+            if(isalpha(num) || isalnum(num)){
+                valid+=tolower(num);
+            }
+        }
+        for(int i=0; i<valid.length() / 2; i++){
+            if(valid[i] != valid[valid.length() - 1 -i]){
+                    return 0;
+            }
+        }
+        return true;
+    }
+};
