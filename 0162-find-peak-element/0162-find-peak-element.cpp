97805397971d0ef5174fe2f91afcb1826c3c0748
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-20 23:24:45 +0530

    Time: 0 ms (100%), Space: 12.7 MB (31.46%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return 0; // Only one element is always the peak
+        if (n == 2) return (nums[0] > nums[1]) ? 0 : 1; // Return index of larger
+
+        for (int i = 1; i <= n - 2; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+
+        // Edge peaks
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        return -1; // This line should never be hit for valid input
+    }
+};
