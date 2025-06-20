a8af3472ab72cfdab8ee858eb2e90647701cc281
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-20 21:55:28 +0530

    Time: 0 ms (100%), Space: 17.9 MB (21.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,14 +1,41 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int maxPrd = nums[0];
+//             int prd = 1;
+//             for (int j = 0; j < nums.size(); j++) {
+//                 prd *= nums[j];
+//                 maxPrd = max(maxPrd, max(prd, nums[j]));
+
+            
+//         }
+//         return maxPrd;
+//     }
+// };
+
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int maxPrd = nums[0];
+        int prd = 1;
+
+        // Forward pass
         for (int i = 0; i < nums.size(); i++) {
-            int prd = 1;
-            for (int j = i; j < nums.size(); j++) {
-                prd *= nums[j];
-                maxPrd = max(maxPrd, prd);
-            }
+            prd *= nums[i];
+            maxPrd = max(maxPrd, prd);
+            if (nums[i] == 0) prd = 1;
         }
+
+        prd = 1;
+
+        // Backward pass
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            prd *= nums[i];
+            maxPrd = max(maxPrd, prd);
+            if (nums[i] == 0) prd = 1;
+        }
+
         return maxPrd;
     }
 };
