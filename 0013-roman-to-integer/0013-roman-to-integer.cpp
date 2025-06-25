9d1eccb89916c25513934cd728e5fd2800bcfd84
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-06-25 19:40:45 +0530

    Time: 6 ms (54.57%), Space: 11.2 MB (71.22%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+    int values[256] = {0}; 
+    values['I'] = 1;
+    values['V'] = 5;
+    values['X'] = 10;
+    values['L'] = 50;
+    values['C'] = 100;
+    values['D'] = 500;
+    values['M'] = 1000;
+
+    int sum = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (values[s[i]] < values[s[i + 1]]) {  
+            sum -= values[s[i]];               
+        } else {
+            sum += values[s[i]];              
+        }
+    }
+    return sum;
+    }
+};
