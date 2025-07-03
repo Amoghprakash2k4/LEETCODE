5af49acbecf1d9438ea26982c9a0805d1df70a93
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-03 13:58:47 +0530

    Time: 0 ms (100%), Space: 11.1 MB (41.75%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,16 +11,62 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* node, vector<int>& arr){
-        if(!node) return;
-        dfs(node->left, arr);
-        dfs(node->right, arr);
-        arr.push_back(node->val);
+    // void dfs(TreeNode* node, vector<int>& arr){
+    //     if(!node) return;
+    //     dfs(node->left, arr);
+    //     dfs(node->right, arr);
+    //     arr.push_back(node->val);
+    // }
+
+    // vector<int> postorderTraversal(TreeNode* root) {
+        // vector<int> arr;
+        // // dfs(root, arr);
+        // // return arr;
+        // // vector<int> arr;
+        // // dfs(root, arr);
+        // // return arr;
+        // if(!root) return arr;
+        // stack<TreeNode*> st;
+        // st.push(root);
+        //  while(!st.empty()){
+        //     TreeNode* node = st.top();
+        //     st.pop();
+
+        //     if(node->right) st.push(node->right);
+            
+        //     if(node->left) st.push(node->left);
+        //     arr.push_back(node->val);
+
+        //  }
+        //  return arr;
+
+        vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> result;
+    if (!root) return result;
+
+    stack<TreeNode*> st;
+    TreeNode* curr = root;
+    TreeNode* lastVisited = nullptr;
+
+    while (curr || !st.empty()) {
+        if (curr) {
+            // Go as left as possible
+            st.push(curr);
+            curr = curr->left;
+        } else {
+            TreeNode* node = st.top();
+            // If right exists and hasn't been processed, move to right
+            if (node->right && lastVisited != node->right) {
+                curr = node->right;
+            } else {
+                // Otherwise, process node
+                result.push_back(node->val);
+                lastVisited = node;
+                st.pop();
+            }
+        }
     }
 
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> arr;
-        dfs(root, arr);
-        return arr;
-    }
-};
+    return result;
+}
+    };
