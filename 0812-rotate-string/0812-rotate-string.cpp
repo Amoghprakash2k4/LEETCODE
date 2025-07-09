4c1b2bd96a0380f416c4cadb7b34668aa1d7b89b
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-09 21:08:02 +0530

    Time: 0 ms (100%), Space: 8 MB (84.04%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,3 +1,24 @@
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         int n = s.size();
+//         if (n != goal.size()) return false;
+
+//         for (int shift = 0; shift < n; shift++) {
+//             bool match = true;
+//             for (int i = 0; i < n; i++) {
+//                 if (goal[i] != s[(i + shift) % n]) {
+//                     match = false;
+//                     break;
+//                 }
+//             }
+//             if (match) return true;
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
