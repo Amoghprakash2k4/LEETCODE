a484665c9df60c6b2e33fe4586961b37c505c899
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-09 21:08:21 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.04%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,29 +1,29 @@
-// class Solution {
-// public:
-//     bool rotateString(string s, string goal) {
-//         int n = s.size();
-//         if (n != goal.size()) return false;
-
-//         for (int shift = 0; shift < n; shift++) {
-//             bool match = true;
-//             for (int i = 0; i < n; i++) {
-//                 if (goal[i] != s[(i + shift) % n]) {
-//                     match = false;
-//                     break;
-//                 }
-//             }
-//             if (match) return true;
-//         }
-//         return false;
-//     }
-// };
-
-
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.length() != goal.length()) return false;
-        string doubled = s + s;
-        return doubled.find(goal) != string::npos ;
+        int n = s.size();
+        if (n != goal.size()) return false;
+
+        for (int shift = 0; shift < n; shift++) {
+            bool match = true;
+            for (int i = 0; i < n; i++) {
+                if (goal[i] != s[(i + shift) % n]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) return true;
+        }
+        return false;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         if (s.length() != goal.length()) return false;
+//         string doubled = s + s;
+//         return doubled.find(goal) != string::npos ;
+//     }
+// };
