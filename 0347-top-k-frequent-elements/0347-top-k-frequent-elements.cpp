3533abbe3466c2d2b49370ae4de4d84bbe0ecce4
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-12 20:36:04 +0530

    Time: 0 ms (100%), Space: 18.1 MB (33.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        for(int num: nums){
-            freq[num]++;
-        }
-        vector<pair<int,int>> freqVec(freq.begin(), freq.end());
-        sort(freqVec.begin(), freqVec.end(), [](auto& a, auto& b){
-            return a.second > b.second;
-        });
-        vector<int> result;
-        for(int i=0; i<k; i++){
-            result.push_back(freqVec[i].first);
-        }
-        return result;
+        // unordered_map<int, int> freq;
+        // for(int num: nums){
+        //     freq[num]++;
+        // }
+        // vector<pair<int,int>> freqVec(freq.begin(), freq.end());
+        // sort(freqVec.begin(), freqVec.end(), [](auto& a, auto& b){
+        //     return a.second > b.second;
+        // });
+        // vector<int> result;
+        // for(int i=0; i<k; i++){
+        //     result.push_back(freqVec[i].first);
+        // }
+        // return result;
+    unordered_map<int,int> freq;
+    for(int num : nums) {
+        freq[num]++;
+    }
+
+    vector<pair<int,int>> freqVec(freq.begin(), freq.end());
+
+    sort(freqVec.begin(), freqVec.end(), [](auto& a, auto& b){
+        return a.second > b.second; // sort by frequency descending
+    });
+
+    vector<int> result;
+    for(int i=0; i<k; i++) {
+        result.push_back(freqVec[i].first);
+    }
+    return result;
+
+
 
        }
 };
