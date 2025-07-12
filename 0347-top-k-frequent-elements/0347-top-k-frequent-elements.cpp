184795f53583681fe4a22e07a27bfd3dd0164061
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-12 20:41:12 +0530

    Time: 0 ms (100%), Space: 18 MB (33.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // unordered_map<int, int> freq;
-        // for(int num: nums){
-        //     freq[num]++;
-        // }
-        // vector<pair<int,int>> freqVec(freq.begin(), freq.end());
-        // sort(freqVec.begin(), freqVec.end(), [](auto& a, auto& b){
-        //     return a.second > b.second;
-        // });
-        // vector<int> result;
-        // for(int i=0; i<k; i++){
-        //     result.push_back(freqVec[i].first);
-        // }
-        // return result;
+    // unordered_map<int,int> freq;
+    // for(int num : nums) {
+    //     freq[num]++;
+    // }
+
+    // vector<pair<int,int>> freqVec(freq.begin(), freq.end());
+
+    // sort(freqVec.begin(), freqVec.end(), [](auto& a, auto& b){
+    //     return a.second > b.second; // sort by frequency descending
+    // });
+
+    // vector<int> result;
+    // for(int i=0; i<k; i++) {
+    //     result.push_back(freqVec[i].first);
+    // }
+    // return result;
     unordered_map<int,int> freq;
     for(int num : nums) {
         freq[num]++;
     }
 
-    vector<pair<int,int>> freqVec(freq.begin(), freq.end());
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> minHeap;
 
-    sort(freqVec.begin(), freqVec.end(), [](auto& a, auto& b){
-        return a.second > b.second; // sort by frequency descending
-    });
+    for(auto& [num, count] : freq) {
+        minHeap.push({count, num});
+        if(minHeap.size() > k) {
+            minHeap.pop();
+        }
+    }
 
     vector<int> result;
-    for(int i=0; i<k; i++) {
-        result.push_back(freqVec[i].first);
+    while(!minHeap.empty()) {
+        result.push_back(minHeap.top().second);
+        minHeap.pop();
     }
     return result;
+}
 
-
-
-       }
 };
