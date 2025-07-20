2f88eda8614fdd936fff35ec995e822543cc41aa
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-20 22:50:57 +0530

    Time: 0 ms (100%), Space: 11.6 MB (29.79%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -6,15 +6,13 @@ public:
             freq[c]++;
         }
 
-        // Convert to vector of pairs to sort
         vector<pair<char, int>> freqVec(freq.begin(), freq.end());
 
-        // Sort by frequency descending
+        
         sort(freqVec.begin(), freqVec.end(), [](auto &a, auto &b) {
             return a.second > b.second;
         });
 
-        // Build result string
         string result;
         for (auto &p : freqVec) {
             result += string(p.second, p.first);
