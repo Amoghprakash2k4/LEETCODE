94cd5b18a78c4e26021a31aa163b3255e40f1dc2
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-07-24 23:10:33 +0530

    Time: 15 ms (77.57%), Space: 23.7 MB (79.59%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,10 +1,12 @@
-
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         if (grid[0][0] != 0 || grid[n-1][n-1] != 0) return -1;
-        vector<vector<int>> dirs = {{0,1},{1,0},{0,-1},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};
+        vector<vector<int>> dirs = {{0,1},{1,0},
+                                    {0,-1},{-1,0},
+                                    {1,1},{-1,-1},
+                                    {1,-1},{-1,1}};
         queue<pair<int, int>> q;
         q.push({0,0});
         grid[0][0] = 1; // use grid to track visited and path length
