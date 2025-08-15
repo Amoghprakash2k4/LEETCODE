57c1a5f6dd4583a8dd72f4ae68e4963888d8a77b
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-16 00:49:35 +0530

    Time: 2 ms (77.33%), Space: 7.7 MB (87.29%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -10,16 +10,26 @@ public:
     // }
 
     int fib(int n) {
-        vector<int> dparr(n + 1, -1); // initialize with -1
+        // vector<int> dparr(n + 1, -1); // initialize with -1
     //     return dp(n, dparr);
     // }
-    if(n<=1) return n;
+    // ----------------
+    // if(n<=1) return n;
 
-    dparr[0]=0;dparr[1]=1;
-    for(int i=2; i<=n; i++){
-        dparr[i] = dparr[i-1]+dparr[i-2];
+    // dparr[0]=0;dparr[1]=1;
+    // for(int i=2; i<=n; i++){
+    //     dparr[i] = dparr[i-1]+dparr[i-2];
+    // }
+    // return dparr[n];
+    // ---------------
+    if (n <= 1) return n;
+    int prev2 = 0, prev1 = 1;
+    for (int i = 2; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
     }
-    return dparr[n];
+    return prev1;
     }
 };
 
