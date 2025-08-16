45c99930651926ee55596839c6dee7cb38e715d2
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-16 20:20:59 +0530

    Time: 0 ms (100%), Space: 12.7 MB (75.76%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        // DP has same structure as triangle
-        vector<vector<int>> dp = triangle;
+        vector<int> front = triangle[n-1];  // start with last row
+        vector<int> curr(n, 0);
 
-        // Bottom-up
         for (int i = n - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                int d  = triangle[i][j] + dp[i+1][j];
-                int dg = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(d, dg);
+                int d  = triangle[i][j] + front[j];
+                int dg = triangle[i][j] + front[j+1];
+                curr[j] = min(d, dg);
             }
+            front = curr;  // ✅ update after finishing row
         }
-        return dp[0][0];
+
+        return front[0];
     }
 };
