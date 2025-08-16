71001b2ce65a646f378edda177532fb69d795fe7
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-16 15:57:39 +0530

    Time: 0 ms (100%), Space: 9.4 MB (8.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
+int countpath(int i, int j,vector<vector<int>>& dp){
+    if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int left = countpath(i-1,j,dp);
+        int right = countpath(i,j-1,dp);
+        return dp[i][j]=left+right;
+}
     int uniquePaths(int m, int n) {
-        // We want to compute C(m+n-2, m-1)
-        long long res = 1;
-        int N = m + n - 2;
-        int k = min(m - 1, n - 1); // smaller of the two to reduce computations
-        for (int i = 1; i <= k; ++i) {
-            res = res * (N - k + i) / i;
-        }
-        return (int)res;
+        // // We want to compute C(m+n-2, m-1)
+        // long long res = 1;
+        // int N = m + n - 2;
+        // int k = min(m - 1, n - 1); 
+        // for (int i = 1; i <= k; ++i) {
+        //     res = res * (N - k + i) / i;
+        // }
+        // return (int)res;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+
+        int i = m-1, j=n-1;
+        return countpath(i,j,dp);
+        
     }
 };
