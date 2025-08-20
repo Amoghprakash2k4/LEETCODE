3ce41a5ea550782d74e741a4d8acaeb8188e8c88
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-21 01:32:11 +0530

    Time: 24 ms (69.11%), Space: 22 MB (53.97%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> prev(amount+1, 0),curr(amount+1, 0);
+
+        for(int i =0 ; i<=amount ; i++){
+            if(i%coins[0] == 0) prev[i]=  i/coins[0];
+            else prev[i] = INT_MAX;
+        }
+
+        for(int ind = 1 ; ind < coins.size(); ind++){
+            for(int t = 0 ; t<= amount ; t++){
+                int nottake = 0+prev[t];
+                int take = INT_MAX;
+                // if(coins[ind] <= t) take = 1 + curr[t-coins[ind]];
+                 if (coins[ind] <= t && curr[t - coins[ind]] != INT_MAX) 
+                        take = 1 + curr[t - coins[ind]];
+
+                curr[t] = min(take,nottake);
+            }
+            prev = curr;
+        }
+        int ans = prev[amount];
+        if(ans == INT_MAX) return -1;
+        else return ans;
+    }
+};
