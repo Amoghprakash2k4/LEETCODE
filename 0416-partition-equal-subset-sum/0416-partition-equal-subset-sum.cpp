2be7360ffc3997e037897344f556688382549bff
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-21 00:19:03 +0530

    Time: 75 ms (85.84%), Space: 13.4 MB (94.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
 bool subsetSumToK(int n, int k, vector<int> &arr) {
     // vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
-    vector<bool> prev(k+1,0), curr(k+1,0);
+    vector<bool> prev(k+1,0);
+    // , curr(k+1,0);
     
-    prev[0]=curr[0] = true;
+    prev[0]= true;
+    // curr[0] =
      if (arr[0] <= k) prev[arr[0]] = true;
 
     // Step 3: Base Case - with only the first element
@@ -12,16 +14,17 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
 
     // Step 4: Fill the DP table
     for (int ind = 1; ind < n; ind++) {
-        curr[0] = true;
-        for (int target = 1; target <= k; target++) {
+        // curr[0] = true;
+        prev[0]= true;
+        for (int target = k; target >= 0; target--) {
             bool notTake = prev[target];
             bool take = false;
             if (arr[ind] <= target) 
                 take =prev[target - arr[ind]];
             
-            curr[target] = take || notTake;
+            prev[target] = take || notTake;
         }
-        prev = curr;
+        // prev = curr;
     }
 
     // Step 5: Final answer
