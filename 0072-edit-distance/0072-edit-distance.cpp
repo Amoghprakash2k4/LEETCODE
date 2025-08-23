dcefc5fc9c37aee618eda5e5c1362bb0e9ce0fc3
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-23 15:55:13 +0530

    Time: 6 ms (66.78%), Space: 10.5 MB (91.23%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -22,24 +22,51 @@ public:
 
     int minDistance(string word1, string word2) {
         int m = word1.size(), n = word2.size();
+        if(m == 0) return n;
+        if(n == 0) return m;
+        // // vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        // for(int i = 0 ; i<=m ; i++) dp[i][0] = i;
+        // for(int i = 0 ; i<=n ; i++) dp[0][i] = i;
 
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        for(int i = 0 ; i<=m ; i++) dp[i][0] = i;
-        for(int i = 0 ; i<=n ; i++) dp[0][i] = i;
+        vector<int> prev(n+1, 0), curr(n+1, 0);
 
-        for(int i =1 ; i <= m ; i++){
+        // for(int i =1 ; i <= m ; i++){
+        //     curr[i] = i;
+        //     for(int j = 1 ; j <= n ; j++){
+        //         if(word1[i-1] == word2[j-1])   
+        //             dp[i][j] = dp[i-1][j-1];
+        //         else{
+        //             int insertOp = dp[i][j-1]; //insert an ectra word, move j and stay i at the same location
+        //             int deleteOp = dp[i-1][j]; // delete the ith letter and search for other possible keeping j same
+        //             int replaceOp = dp[i-1][j-1]; // replace i with jthe word and move both i and j
+
+        //              dp[i][j] = 1 + min(insertOp, min(deleteOp, replaceOp));
+        //         }
+        //     }
+        // }
+        // return dp[m][n];
+
+             for(int j = 0 ; j<=n ; j++) prev[j] = j;
+
+            for(int i =1 ; i <= m ; i++){
+            curr[0] = i;
             for(int j = 1 ; j <= n ; j++){
                 if(word1[i-1] == word2[j-1])   
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 else{
-                    int insertOp = dp[i][j-1]; //insert an ectra word, move j and stay i at the same location
-                    int deleteOp = dp[i-1][j]; // delete the ith letter and search for other possible keeping j same
-                    int replaceOp = dp[i-1][j-1]; // replace i with jthe word and move both i and j
+                    int insertOp = curr[j-1]; //insert an ectra word, move j and stay i at the same location
+                    int deleteOp = prev[j]; // delete the ith letter and search for other possible keeping j same
+                    int replaceOp = prev[j-1]; // replace i with jthe word and move both i and j
 
-                     dp[i][j] = 1 + min(insertOp, min(deleteOp, replaceOp));
+                     curr[j] = 1 + min(insertOp, min(deleteOp, replaceOp));
                 }
             }
+            prev = curr;
         }
-        return dp[m][n];
+        return curr[n];
+
+
+
+
     }
 };
