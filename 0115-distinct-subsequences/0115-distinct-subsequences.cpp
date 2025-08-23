165ab293fb3fadb4f8a3a374a7e60db8f7141ea4
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-23 14:45:33 +0530

    Time: 7 ms (96.98%), Space: 9 MB (86.8%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -6,7 +6,7 @@ public:
         dp[0] = 1; // empty t is always subsequence
 
         for (int i = 1; i <= m; i++) {
-            // Traverse backwards to avoid overwriting previous states
+            
             for (int j = n; j >= 1; j--) {
                 if (s[i - 1] == t[j - 1]) {
                     dp[j] += dp[j - 1];
