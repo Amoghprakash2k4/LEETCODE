7c1cac57b2ceb37dd1298a8ace9a8aaa6b346a33
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-24 15:42:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (76.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -43,27 +43,27 @@ public:
         // return dp[m-1][n-1];
 
         // --------------------------------------
-        vector<int>prev(n,0);
-        for (int i = 0; i < m; i++) {
-            vector<int> curr(n,0);
-            for (int j = 0; j < n; j++) {
-                
-                // Base case: starting cell
-                if (i == 0 && j == 0) {
-                    curr[j] = 1;
-                } else {
-                    int up = 0, left = 0;
-                    if (i > 0) up = prev[j];
-                    if (j > 0) left = curr[j-1];
-                    curr[j] = up + left;
+            vector<int>prev(n,0),  curr(n,0);
+            for (int i = 0; i < m; i++) {
+                // vector<int> curr(n,0);
+                for (int j = 0; j < n; j++) {
+                    
+                    // Base case: starting cell
+                    if (i == 0 && j == 0) {
+                        curr[j] = 1;
+                    } else {
+                        int up = 0, left = 0;
+                        if (i > 0) up = prev[j];
+                        if (j > 0) left = curr[j-1];
+                        curr[j] = up + left;
+                    }
+                    
                 }
+                prev = curr;
                 
             }
-            prev = curr;
-            
-        }
 
-        return prev[n-1];
+            return prev[n-1];
 
 
         }
