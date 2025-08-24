8e4f4d28a9bd7b4c4cac966f73b3daa9d61d5d13
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-24 15:43:29 +0530

    Time: 0 ms (100%), Space: 11.4 MB (94.05%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,10 +4,10 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
         
-        vector<int> prev(n, 0);
+        vector<int> prev(n, 0),curr(n, 0);
         
         for (int i = 0; i < m; i++) {
-            vector<int> curr(n, 0);
+            // vector<int> curr(n, 0);
             for (int j = 0; j < n; j++) {
                 if (obstacleGrid[i][j] == 1) {
                     curr[j] = 0;
