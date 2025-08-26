5f4293f91ff7fc697ed9bae5d73ec0477cc497d0
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-27 00:53:48 +0530

    Time: 7 ms (21.4%), Space: 19.6 MB (38.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int profit(vector<int>& prices, int n, int buy, int ind, vector<vector<int>>& dp) {
-        if (ind == n) return 0;
+    // int profit(vector<int>& prices, int n, int buy, int ind, vector<vector<int>>& dp) {
+    //     if (ind == n) return 0;
 
-        if (dp[ind][buy] != -1) return dp[ind][buy];
+    //     if (dp[ind][buy] != -1) return dp[ind][buy];
 
-        if (buy) {
-            // Either buy today or skip
-            dp[ind][buy] = max(-prices[ind] + profit(prices, n, 0, ind + 1, dp),
-                                profit(prices, n, 1, ind + 1, dp));
-        } else {
-            // Either sell today or skip
-            dp[ind][buy] = max(prices[ind] + profit(prices, n, 1, ind + 1, dp),
-                                profit(prices, n, 0, ind + 1, dp));
-        }
+    //     if (buy) {
+    //         // Either buy today or skip
+    //         dp[ind][buy] = max(-prices[ind] + profit(prices, n, 0, ind + 1, dp),
+    //                             profit(prices, n, 1, ind + 1, dp));
+    //     } else {
+    //         // Either sell today or skip
+    //         dp[ind][buy] = max(prices[ind] + profit(prices, n, 1, ind + 1, dp),
+    //                             profit(prices, n, 0, ind + 1, dp));
+    //     }
 
-        return dp[ind][buy];
-    }
+    //     return dp[ind][buy];
+    // }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));  // dp[ind][buy]
-        return profit(prices, n, 1, 0, dp);  // start at day 0, buy allowed
+        
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));  // dp[ind][buy]
+        dp[n][0]=0,dp[n][1]=0;
+        for(int ind = n-1 ; ind >= 0 ; ind--){
+            for(int buy=0 ; buy<=1 ; buy++){
+                if (buy) {
+            // Either buy today or skip
+            dp[ind][buy] = max(-prices[ind] + dp[ind+1][0],
+                               dp[ind+1][1]);
+        } else {
+            // Either sell today or skip
+            dp[ind][buy] = max(prices[ind] + dp[ind+1][1],
+                               dp[ind+1][0]);
+        }
+            }
+        }
+        return dp[0][1];
     }
 };
