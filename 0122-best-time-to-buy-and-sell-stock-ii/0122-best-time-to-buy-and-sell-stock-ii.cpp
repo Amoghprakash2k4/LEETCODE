8368591a2f0e5d535850abe614fca9eea51fd2fd
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-27 00:58:43 +0530

    Time: 1 ms (44.33%), Space: 17.4 MB (42.87%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -21,21 +21,23 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         
-        vector<vector<int>> dp(n+1, vector<int>(2, -1));  // dp[ind][buy]
-        dp[n][0]=0,dp[n][1]=0;
+        // vector<vector<int>> dp(n+1, vector<int>(2, -1));  // dp[ind][buy]
+        vector<int> ahead(2,0), cur(2,0);
+        ahead[0]=0,ahead[1]=0;
         for(int ind = n-1 ; ind >= 0 ; ind--){
             for(int buy=0 ; buy<=1 ; buy++){
                 if (buy) {
             // Either buy today or skip
-            dp[ind][buy] = max(-prices[ind] + dp[ind+1][0],
-                               dp[ind+1][1]);
+            cur[buy] = max(-prices[ind] + ahead[0],
+                               ahead[1]);
         } else {
             // Either sell today or skip
-            dp[ind][buy] = max(prices[ind] + dp[ind+1][1],
-                               dp[ind+1][0]);
+            cur[buy] = max(prices[ind] + ahead[1],
+                               ahead[0]);
         }
+        ahead = cur;
             }
         }
-        return dp[0][1];
+        return ahead[1];
     }
 };
