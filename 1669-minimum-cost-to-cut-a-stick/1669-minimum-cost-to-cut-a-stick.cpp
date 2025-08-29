192fd60259e082a503b7e592832173dab468ae10
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-29 23:24:05 +0530

    Time: 16 ms (88.21%), Space: 13.9 MB (78.42%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
 
-    int f(int i, int j, vector<int>& cuts,vector<vector<int>> &dp){
-        if(i > j) return 0;
-        int mini = INT_MAX;
-        if(dp[i][j] != -1) return dp[i][j];
-        for(int ind=i ; ind <=j ; ind++){
-            int cost = cuts[j+1] - cuts[i-1] + f(i, ind-1, cuts, dp) +f(ind+1, j, cuts,dp);
-            mini = min(mini, cost);
-        }
-        return dp[i][j] =mini;
-    }
+    // int f(int i, int j, vector<int>& cuts,vector<vector<int>> &dp){
+    //     if(i > j) return 0;
+    //     int mini = INT_MAX;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     for(int ind=i ; ind <=j ; ind++){
+    //         int cost = cuts[j+1] - cuts[i-1] + f(i, ind-1, cuts, dp) +f(ind+1, j, cuts,dp);
+    //         mini = min(mini, cost);
+    //     }
+    //     return dp[i][j] =mini;
+    // }
 
 
     int minCost(int n, vector<int>& cuts) {
@@ -18,9 +18,23 @@ public:
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0); //go get the length of the stick
         sort(cuts.begin(), cuts.end()); 
-        vector<vector<int>> dp(c+1, vector<int>(c+1,-1)); 
+        vector<vector<int>> dp(c+2, vector<int>(c+2,0)); 
 
-        return f(1,c,cuts,dp);
+        for(int i=c ; i>=1 ; i--)
+        {
+            for(int j=1 ; j<=c ; j++)
+            {
+                if(i > j) continue;
+                int mini = INT_MAX;
+                // if(dp[i][j] != -1) return dp[i][j];
+                for(int ind=i ; ind <=j ; ind++){
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] +dp[ind+1][j];
+                    mini = min(mini, cost);
+            }
+            dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
 
     }
 };
