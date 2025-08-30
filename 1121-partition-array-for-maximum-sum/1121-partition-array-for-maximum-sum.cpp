38c1c49cb9136e27b808f19c52d0a74382126578
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-08-30 20:05:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (61.77%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.cpp
@@ -18,8 +18,23 @@ public:
     }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        int n = arr.size();
-        vector<int> dp(n+1, -1);
-        return f(0, arr, k , n, dp);
+    int n = arr.size();
+    vector<int> dp(n+1, 0); // dp[n] = 0 already
+    
+    for(int i=n-1 ; i>=0 ; i--){
+        int len = 0;
+        int maxi = INT_MIN;
+        int maxAns = 0;  // <-- fix: start with 0 instead of INT_MIN
+
+        for(int j = i; j < min(i + k, n); j++) {
+            len++;
+            maxi = max(maxi, arr[j]);  
+            int sum = len * maxi + dp[j+1];  
+            maxAns = max(maxAns, sum); 
+        }
+        dp[i] = maxAns;
+    }
+    return dp[0];
+
     }
 };
