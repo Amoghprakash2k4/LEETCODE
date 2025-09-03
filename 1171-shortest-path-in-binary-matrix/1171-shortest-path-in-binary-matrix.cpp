11b261c11bede39b5d5d35bdf85b8942fb3b5eca
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-09-04 00:55:23 +0530

    Time: 19 ms (58.71%), Space: 26.2 MB (47.65%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -3,45 +3,37 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+        if(grid[0][0] == 1 || grid[n-1][m-1]== 1) return -1;
 
-        // If start or end cell is blocked
-        if (grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
-
-        // Distance matrix
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        dist[0][0] = 1;  // starting cell counts as step 1
+        dist[0][0] = 1;
 
-        // BFS queue -> {distance, {row, col}}
         queue<pair<int, pair<int,int>>> q;
-        q.push({1, {0,0}});
+        q.push({1,{0,0}});
 
-        // 8 directions
-        vector<vector<int>> dirs = {{0,1},{1,0},
-                                    {0,-1},{-1,0},
-                                    {1,1},{-1,-1},
-                                    {1,-1},{-1,1}};
-
-        while (!q.empty()) {
-            auto it = q.front();
+        while(!q.empty())
+        {
+            auto node = q.front();
             q.pop();
 
-            int dis = it.first;
-            int r = it.second.first;
-            int c = it.second.second;
+            int d = node.first;   // \U0001f539 renamed
+            int x = node.second.first;
+            int y = node.second.second;
 
-            // If destination reached
-            if (r == n-1 && c == m-1) return dis;
+            if (x == n-1 && y == m-1) return d;
 
-            // Explore 8 neighbors
-            for (auto &d : dirs) {
-                int newr = r + d[0];
-                int newc = c + d[1];
+            for(int i= -1 ; i<=1 ; i++)
+            {
+                for(int j=-1 ; j<=1 ; j++)
+                {
+                    int newr = x+i;
+                    int newc = y+j;
 
-                if (newr >= 0 && newr < n && newc >= 0 && newc < m &&
-                    grid[newr][newc] == 0 && dis + 1 < dist[newr][newc]) {
-                    
-                    dist[newr][newc] = dis + 1;
-                    q.push({dis + 1, {newr, newc}});
+                    if(newr>=0 && newr < n && newc >=0 && newc < m && grid[newr][newc] == 0 && d + 1 < dist[newr][newc])
+                    {
+                        dist[newr][newc] = d + 1;
+                        q.push({d + 1, {newr, newc}});
+                    }
                 }
             }
         }
