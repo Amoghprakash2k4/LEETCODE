70e5cfe3356f117278aa47ab8a5ba84cecc5c894
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-09-07 22:10:17 +0530

    Time: 7 ms (64.03%), Space: 17.7 MB (62.52%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -6,17 +6,29 @@ public:
 
         vector<vector<int>> dp(n+1, vector<int>(2*k+1, 0));
 // basically the logic used here is no need to keep track of buy and cap separately , intead keep one variable transaction and whenever the number is even it is buy, like 0,2. And when it is odd it is sell.
-        for (int ind = n-1; ind >= 0; ind--) {
-            for (int transaction = 2*k-1; transaction >= 0; transaction--) {
-                if (transaction % 2 == 0) { // buy
-                    dp[ind][transaction] = max(-prices[ind] + dp[ind+1][transaction+1],
-                                               dp[ind+1][transaction]);
-                } else { // sell
-                    dp[ind][transaction] = max(prices[ind] + dp[ind+1][transaction+1],
-                                               dp[ind+1][transaction]);
+        // for (int ind = n-1; ind >= 0; ind--) {
+        //     for (int transaction = 2*k-1; transaction >= 0; transaction--) {
+        //         if (transaction % 2 == 0) { // buy
+        //             dp[ind][transaction] = max(-prices[ind] + dp[ind+1][transaction+1],
+        //                                        dp[ind+1][transaction]);
+        //         } else { // sell
+        //             dp[ind][transaction] = max(prices[ind] + dp[ind+1][transaction+1],
+        //                                        dp[ind+1][transaction]);
+        //         }
+        //     }
+        // }
+        // return dp[0][0];   
+
+        for(int ind=n-1 ; ind >=0 ; ind--){
+            for(int target=2*k-1 ; target >=0 ; target--){
+                if((target%2) == 0){
+                    dp[ind][target]= max(-prices[ind]+dp[ind+1][target+1], dp[ind+1][target]);
                 }
-            }
+                else{
+                    dp[ind][target] = max(prices[ind]+dp[ind+1][target+1], dp[ind+1][target]);
+                }
+            }   
         }
-        return dp[0][0];   
+        return dp[0][0];
     }
 };
