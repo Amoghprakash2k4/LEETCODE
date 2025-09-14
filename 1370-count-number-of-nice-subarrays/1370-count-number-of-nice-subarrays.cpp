afeb3d1e74b8d7b5959d43c3df7a6c093c22cab4
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-09-14 23:14:52 +0530

    Time: 5 ms (73.35%), Space: 71.5 MB (68.56%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1);
+    int numberOfSubarrays(vector<int>& nums, int goal) {
+        return f(nums, goal) - f(nums, goal-1);
+
     }
 
-    int atMost(vector<int>& nums, int k) {
-        int left = 0, right = 0;
-        int count = 0;
-        int ans = 0;
+    int f(vector<int>& nums, int goal){
+        int left=0 , count =0, right=0;
+        int sum=0;
 
-        while (right < nums.size()) {
-            // If current number is odd, reduce k
-            if (nums[right] % 2 == 1) {
-                k--;
-            }
-
-            // While we exceed the allowed odd numbers, move left forward
-            while (k < 0) {
-                if (nums[left] % 2 == 1) {
-                    k++;
-                }
+        while(right <nums.size()){
+            sum+=(nums[right] % 2);
+            while(sum > goal && left<=right){
+                sum-=(nums[left] % 2);
                 left++;
             }
-
-            // All subarrays ending at 'right' and starting from [left, right] are valid
-            ans += right - left + 1;
-
+            count+= right-left+1;
             right++;
         }
-
-        return ans;
+        return count;
     }
+    
 };
