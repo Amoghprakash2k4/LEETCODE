8b0b822ef866626c3b643a3e81f4b6cb4bd0d54f
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-09-18 00:32:18 +0530

    Time: 1 ms (94.34%), Space: 35.2 MB (66.05%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,49 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        const long long MOD = 1e9 + 7;
-        
-        // Step 1: Build adjacency list
-        vector<vector<pair<int, long long>>> graph(n);
-        for (auto &r : roads) {
-            int u = r[0], v = r[1];
-            long long time = r[2];
-            graph[u].push_back({v, time});
-            graph[v].push_back({u, time});
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto &it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
         }
 
-        // Step 2: Initialize distance and ways arrays
-        vector<long long> dist(n, LLONG_MAX);
+        priority_queue<pair<long long, int>, vector<pair<long long,int>>, greater<pair<long long,int>>> q;
+
+        vector<long long> dis(n, LLONG_MAX);
         vector<long long> ways(n, 0);
-        dist[0] = 0;
+        int mod = 1e9 + 7;
+
+        dis[0] = 0;
         ways[0] = 1;
+        q.push({0, 0});
 
-        // Step 3: Min-heap for Dijkstra
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
-        pq.push({0, 0});  // {distance, node}
+        while (!q.empty()) {
+            auto [di, node] = q.top();
+            q.pop();
 
-        while (!pq.empty()) {
-            auto [currDist, node] = pq.top();
-            pq.pop();
+            if (di > dis[node]) continue; // skip outdated
 
-            if (currDist > dist[node]) continue;
+            for (auto [adnode, d] : adj[node]) {
+                long long newDist = di + d;
 
-            for (auto [nei, time] : graph[node]) {
-                long long newDist = currDist + time;
-
-                // Case 1: Found a shorter path
-                if (newDist < dist[nei]) {
-                    dist[nei] = newDist;
-                    ways[nei] = ways[node];
-                    pq.push({newDist, nei});
-                }
-                // Case 2: Found another shortest path
-                else if (newDist == dist[nei]) {
-                    ways[nei] = (ways[nei] + ways[node]) % MOD;
+                if (newDist < dis[adnode]) {
+                    dis[adnode] = newDist;
+                    ways[adnode] = ways[node];
+                    q.push({dis[adnode], adnode});
+                } 
+                else if (newDist == dis[adnode]) {
+                    ways[adnode] = (ways[adnode] + ways[node]) % mod;
                 }
             }
         }
 
-        return ways[n - 1] % MOD;
+        return ways[n - 1] % mod;
     }
 };
