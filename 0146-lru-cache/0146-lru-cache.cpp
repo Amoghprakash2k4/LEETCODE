305d3e78f0da95a634b9a69f6bad18bf16f85eb8
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-09-20 15:46:16 +0530

    Time: 64 ms (81.41%), Space: 173.1 MB (72.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,74 +1,84 @@
+// class LRUCache {
+// private:
+//     int capacity;
+//     list<pair<int, int>> cache; // stores {key, value}
+//     unordered_map<int, list<pair<int, int>>::iterator> map;
+
+// public:
+//     LRUCache(int capacity) {
+//         this->capacity = capacity;
+//     }
+
+//     int get(int key) {
+//         auto it = map.find(key);
+//         if (it == map.end()) return -1; // not found
+
+//         // Move the accessed node to the front (most recently used)
+//         cache.splice(cache.begin(), cache, it->second);
+//         return it->second->second; // return value
+//     }
+
+//     void put(int key, int value) {
+//         auto it = map.find(key);
+
+//         if (it != map.end()) {
+//             // Update the value and move to front
+//             it->second->second = value;
+//             cache.splice(cache.begin(), cache, it->second);
+//         } else {
+//             // If cache is full, remove least recently used
+//             if (cache.size() == capacity) {
+//                 auto lru = cache.back();
+//                 map.erase(lru.first);
+//                 cache.pop_back();
+//             }
+//             // Insert new node at front
+//             cache.emplace_front(key, value);
+//             map[key] = cache.begin();
+//         }
+//     }
+// };
+
+
+
 class LRUCache {
-  public:
-    class node {
-      public:
-        int key;
-      int val;
-      node * next;
-      node * prev;
-      node(int _key, int _val) {
-        key = _key;
-        val = _val;
-      }
-    };
+private:
+    int capacity;
+    list<pair<int, int>> cache;
+    unordered_map<int, list<pair<int,int>>::iterator> map;
 
-  node * head = new node(-1, -1);
-  node * tail = new node(-1, -1);
-
-  int cap;
-  unordered_map < int, node * > m;
-
-  LRUCache(int capacity) {
-    cap = capacity;
-    head -> next = tail;
-    tail -> prev = head;
-  }
-
-  void addnode(node * newnode) {
-    node * temp = head -> next;
-    newnode -> next = temp;
-    newnode -> prev = head;
-    head -> next = newnode;
-    temp -> prev = newnode;
-  }
-
-  void deletenode(node * delnode) {
-    node * delprev = delnode -> prev;
-    node * delnext = delnode -> next;
-    delprev -> next = delnext;
-    delnext -> prev = delprev;
-  }
-
-  int get(int key_) {
-    if (m.find(key_) != m.end()) {
-      node * resnode = m[key_];
-      int res = resnode -> val;
-      m.erase(key_);
-      deletenode(resnode);
-      addnode(resnode);
-      m[key_] = head -> next;
-      return res;
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
     }
+    
+    int get(int key) {
+        auto it = map.find(key);
+        if(it == map.end()) return -1;
 
-    return -1;
-  }
-
-  void put(int key_, int value) {
-    if (m.find(key_) != m.end()) {
-      node * existingnode = m[key_];
-      m.erase(key_);
-      deletenode(existingnode);
+        cache.splice(cache.begin(), cache, it->second);
+        return it->second->second;
     }
-    if (m.size() == cap) {
-      m.erase(tail -> prev -> key);
-      deletenode(tail -> prev);
-    }
+    
+    void put(int key, int value) {
+        auto it = map.find(key);
+        if(it != map.end())
+        {
+            it->second->second = value;
+            cache.splice(cache.begin(), cache, it->second);
+        }
+        else{
+            if( cache.size() == capacity){
+                auto lru =  cache.back();
+                map.erase(lru.first);
+                cache.pop_back();
 
-    addnode(new node(key_, value));
-    m[key_] = head -> next;
-  }
+            }
+            cache.emplace_front(key, value);
+            map[key] = cache.begin();
+        }
+    }
 };
- 
 
 /**
  * Your LRUCache object will be instantiated and called as such:
