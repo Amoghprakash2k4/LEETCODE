5f4ab456c57d3a8034f74c95aef09a9bbef583fc
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-09-20 16:09:17 +0530

    Time: 3 ms (83.65%), Space: 10.2 MB (37.61%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,21 +1,52 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        if (n == 1) return "1";
+        // if (n == 1) return "1";
         
-        string prev = countAndSay(n - 1);  // get the sequence for n-1
-        string result = "";
+        // string prev = countAndSay(n - 1);  // get the sequence for n-1
+        // string result = "";
         
-        int count = 1;  
-        for (int i = 1; i <= prev.size(); i++) {
-            if (i < prev.size() && prev[i] == prev[i - 1]) {
-                count++;  // count same consecutive digits
-            } else {
-                result += to_string(count) + prev[i - 1];  // add "count + digit"
-                count = 1;  // reset counter
-            }
-        }
+        // int count = 1;  
+        // for (int i = 1; i <= prev.size(); i++) {
+        //     if (i < prev.size() && prev[i] == prev[i - 1]) {
+        //         count++;  // count same consecutive digits
+        //     } else {
+        //         result += to_string(count) + prev[i - 1];  // add "count + digit"
+        //         count = 1;  // reset counter
+        //     }
+        // }
         
-        return result;
+        // return result;
+        if(n==1) return "1";
+
+string prev = countAndSay(n-1);
+string result = "";
+int count =1;
+for(int i=1 ; i<prev.size() ; i++){
+    if(i<prev.size() && prev[i] == prev[i-1])
+        count++;
+    else{
+        result+= to_string(count) + prev[i-1];
+        count = 1;
+    }
+}
+result += to_string(count) + prev.back();
+return result;
     }
 };
+
+
+// if(n==1) return "1";
+
+// string prev = countAndSay(n-1);
+// string result = "";
+
+// for(int i=1 ; i<prev.size() ; i++){
+//     if(i<prev.size() && prev[i] == prev[i-1])
+//         count++;
+//     else{
+//         result+= to_string(count) + prev[i-1];
+//         count = 1;
+//     }
+// }
+// return result;
