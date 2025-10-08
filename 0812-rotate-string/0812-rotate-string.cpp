6988d7b15c0750c1a2a272f57c3f1c5193f8619c
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-10-08 22:51:41 +0530

    Time: 0 ms (100%), Space: 8.2 MB (36.62%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -3,6 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) return false;
         string doubled = s + s;
-        return doubled.find(goal) != string::npos ;
+        return doubled.find(goal) != string::npos;
+        // != string::npos ;
     }
 };
