a6fea581de19829722c4e4185537a69fb05182da
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-06 02:34:40 +0530

    Time: 0 ms (100%), Space: 11.7 MB (19.46%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     string frequencySort(string s) {
+        // unordered_map<char, int> freq;
+        // for (char c : s) {
+        //     freq[c]++;
+        // }
+
+        // vector<pair<char, int>> freqVec(freq.begin(), freq.end());
+
+        
+        // sort(freqVec.begin(), freqVec.end(), [](auto &a, auto &b) {
+        //     return a.second > b.second;
+        // });
+
+        // string result;
+        // for (auto p : freqVec) {
+        //     result += string(p.second, p.first);
+        // }
+
+        // return result;
+
         unordered_map<char, int> freq;
-        for (char c : s) {
+        for(auto c: s){
             freq[c]++;
         }
 
-        vector<pair<char, int>> freqVec(freq.begin(), freq.end());
+        vector<pair<char,int>> freqvec(freq.begin(), freq.end());
 
-        
-        sort(freqVec.begin(), freqVec.end(), [](auto &a, auto &b) {
+        sort(freqvec.begin(), freqvec.end(), [](auto& a, auto& b){
             return a.second > b.second;
         });
 
+
         string result;
-        for (auto p : freqVec) {
+        for(auto &p : freqvec){
             result += string(p.second, p.first);
         }
 
         return result;
+
     }
 };
