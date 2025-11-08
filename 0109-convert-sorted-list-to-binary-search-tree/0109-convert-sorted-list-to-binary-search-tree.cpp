cce0b8fe00a849e3f3d31e4d92a44b96a8b05a77
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-09 00:36:48 +0530

    Time: 5 ms (44.55%), Space: 33.1 MB (40.54%) - LeetHub

---
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        // base case
+        if (!head) return nullptr;
+        if (!head->next) return new TreeNode(head->val);
+
+        // find middle node
+        ListNode* prev = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // disconnect left half
+        if (prev) prev->next = nullptr;
+
+        // create root from middle
+        TreeNode* root = new TreeNode(slow->val);
+
+        // recursively build left and right subtrees
+        root->left = sortedListToBST(head == slow ? nullptr : head);
+        root->right = sortedListToBST(slow->next);
+
+        return root;
+    }
+};
