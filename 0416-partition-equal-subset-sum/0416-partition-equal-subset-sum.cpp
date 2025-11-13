845ab9c639c772184fd5d0cf1e148f1a95d1a8d1
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-13 23:56:39 +0530

    Time: 143 ms (41.16%), Space: 17.5 MB (60.06%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,34 +1,57 @@
 class Solution {
 public:
 bool subsetSumToK(int n, int k, vector<int> &arr) {
-    // vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
-    vector<bool> prev(k+1,0);
-    // , curr(k+1,0);
-    
-    prev[0]= true;
-    // curr[0] =
-     if (arr[0] <= k) prev[arr[0]] = true;
+    vector<vector<bool>> dp(n, vector<bool>(k+1, false));
 
-    // Step 3: Base Case - with only the first element
-    // if (arr[0] <= k) dp[0][arr[0]] = true;
-
-    // Step 4: Fill the DP table
-    for (int ind = 1; ind < n; ind++) {
-        // curr[0] = true;
-        prev[0]= true;
-        for (int target = k; target >= 0; target--) {
-            bool notTake = prev[target];
-            bool take = false;
-            if (arr[ind] <= target) 
-                take =prev[target - arr[ind]];
-            
-            prev[target] = take || notTake;
-        }
-        // prev = curr;
+    for(int i=0 ; i<n ; i++){
+        dp[i][0] = true;
     }
 
-    // Step 5: Final answer
-    return prev[k];
+    if(arr[0] <= k) 
+        dp[0][arr[0]] = true;
+
+    for(int ind = 1 ; ind <arr.size() ; ind++){
+        for(int target = 1 ; target<=k ; target++){
+            bool nottake = dp[ind-1][target];
+
+            bool take = false;
+            if(arr[ind] <= target)
+                take = dp[ind-1][target - arr[ind]];
+
+            dp[ind][target] = take || nottake;
+
+        }
+        
+    }
+    return dp[n-1][k];
+    // // vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
+    // vector<bool> prev(k+1,0);
+    // // , curr(k+1,0);
+    
+    // prev[0]= true;
+    // // curr[0] =
+    //  if (arr[0] <= k) prev[arr[0]] = true;
+
+    // // Step 3: Base Case - with only the first element
+    // // if (arr[0] <= k) dp[0][arr[0]] = true;
+
+    // // Step 4: Fill the DP table
+    // for (int ind = 1; ind < n; ind++) {
+    //     // curr[0] = true;
+    //     prev[0]= true;
+    //     for (int target = k; target >= 0; target--) {
+    //         bool notTake = prev[target];
+    //         bool take = false;
+    //         if (arr[ind] <= target) 
+    //             take =prev[target - arr[ind]];
+            
+    //         prev[target] = take || notTake;
+    //     }
+    //     // prev = curr;
+    // }
+
+    // // Step 5: Final answer
+    // return prev[k];
 
     // vector<bool> prev(k+1,false);
     // prev[0] = true;
