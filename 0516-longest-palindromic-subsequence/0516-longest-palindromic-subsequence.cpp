3706d91cbffc45e1f2d4bd371d848a1d6bef793f
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-14 21:23:50 +0530

    Time: 87 ms (42.62%), Space: 75.9 MB (42.58%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -4,20 +4,33 @@ int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
 
-        vector<int> prev(n + 1, 0), curr(n + 1, 0);
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    curr[j] = prev[j - 1] + 1;   
-                } else {
-                    curr[j] = max(prev[j], curr[j - 1]); // mismatch → max(top, left)
-                }
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        for(int i=m-1 ; i>=0 ; i--){
+            for(int j=n-1 ; j>=0 ; j--){
+                if(text1[i] == text2[j])
+                    dp[i][j] = 1+dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
             }
-            prev = curr; // move current row to previous row
         }
+        return dp[0][0];
 
-        return prev[n];
+
+
+        // vector<int> prev(n + 1, 0), curr(n + 1, 0);
+
+        // for (int i = 1; i <= m; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         if (text1[i - 1] == text2[j - 1]) {
+        //             curr[j] = prev[j - 1] + 1;   
+        //         } else {
+        //             curr[j] = max(prev[j], curr[j - 1]); // mismatch → max(top, left)
+        //         }
+        //     }
+        //     prev = curr; // move current row to previous row
+        // }
+
+        // return prev[n];
 }
     int longestPalindromeSubseq(string s) {
         string t = s;
