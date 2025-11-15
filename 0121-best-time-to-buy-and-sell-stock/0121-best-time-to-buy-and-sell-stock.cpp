94064277feb2bda84dfca99bf78d75558b675254
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-15 22:27:30 +0530

    Time: 0 ms (100%), Space: 97.3 MB (60.96%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
-
-        // Base case: at the end, profit is 0 whether we can buy or not
-        dp[n][0] = dp[n][1] = 0;
-
-        // Fill DP table backwards
-        for (int ind = n - 1; ind >= 0; ind--) {
-            // If we can buy
-            dp[ind][1] = max(-prices[ind] + dp[ind + 1][0], dp[ind + 1][1]);
-
-            // If we can sell (only once)
-            dp[ind][0] = max(prices[ind], dp[ind + 1][0]);
+        int minprize=prices[0];
+        int maxprofit=0;
+        for(int i=1; i< prices.size(); i++){
+            int currprofit = prices[i]-minprize;        //profit ill make by buying mini and selling on ith day
+            maxprofit = max(maxprofit, currprofit);
+            minprize = min(minprize, prices[i]);
         }
-
-        // Start with ability to buy on day 0
-        return dp[0][1];
+        return maxprofit;
     }
 };
