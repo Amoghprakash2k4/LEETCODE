302011656f206d0522cd4f7699e4ba8466d78d0f
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-15 22:24:27 +0530

    Time: 399 ms (40.54%), Space: 211.6 MB (31.84%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,52 +1,52 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0 )));
-    vector<vector<int>> after(2, vector<int>(3,0 ));
-    vector<vector<int>> curr(2, vector<int>(3,0 ));
-        for(int ind = n-1 ; ind >= 0 ; ind--){
-            for(int buy=0 ; buy <= 1 ; buy++){
-                for(int cap=1; cap <= 2 ; cap++){
-                    if(buy){
-                        curr[buy][cap] = max(-prices[ind]+ after[0][cap], 
-                                                after[1][cap]);
-                    }
-                    else{
-                        curr[buy][cap] = max(prices[ind]+ after[1][cap-1], 
-                                                after[0][cap]);
-                    }
-                }
-                after = curr;
-            }
-        }
-        return curr[1][2];
-}    
-};
-
-
 // class Solution {
 // public:
 //     int maxProfit(vector<int>& prices) {
 //         int n = prices.size();
-//         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
-
+//         // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>  (3,0 )));
+//     vector<vector<int>> after(2, vector<int>(3,0 ));
+//     vector<vector<int>> curr(2, vector<int>(3,0 ));
 //         for(int ind = n-1 ; ind >= 0 ; ind--){
 //             for(int buy=0 ; buy <= 1 ; buy++){
-//                 for (int cap = 0; cap <= 2; cap++) {
-//                     if (buy) {
-//                         dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
-//                                                 dp[ind+1][1][cap]);
-//                     } else {
-//                         if (cap > 0)
-//                             dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],
-//                                                     dp[ind+1][0][cap]);
-//                         else
-//                             dp[ind][buy][cap] = dp[ind+1][0][cap]; // no transactions left
+//                 for(int cap=1; cap <= 2 ; cap++){
+//                     if(buy){
+//                         curr[buy][cap] = max(-prices[ind]+ after[0][cap], 
+//                                                 after[1][cap]);
+//                     }
+//                     else{
+//                         curr[buy][cap] = max(prices[ind]+ after[1][cap-1], 
+//                                                 after[0][cap]);
 //                     }
 //                 }
+//                 after = curr;
 //             }
 //         }
-//         return dp[0][1][2];
-//     }    
+//         return curr[1][2];
+// }    
 // };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3,0)));
+
+        for(int ind = n-1 ; ind >= 0 ; ind--){
+            for(int buy=0 ; buy <= 1 ; buy++){
+                for (int cap = 0; cap <= 2; cap++) {
+                    if (buy) {
+                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
+                                                dp[ind+1][1][cap]);
+                    } else {
+                        if (cap > 0)
+                            dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],
+                                                    dp[ind+1][0][cap]);
+                        else
+                            dp[ind][buy][cap] = dp[ind+1][0][cap]; // no transactions left
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
+    }    
+};
