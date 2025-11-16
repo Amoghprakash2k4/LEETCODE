2bb95bc847b3184fa2f20d7819ac94e11deca09d
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-17 01:40:00 +0530

    Time: 0 ms (100%), Space: 15.1 MB (18.55%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,29 +1,72 @@
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int m = matrix[0].size();
+//         int n = matrix.size();
+//         vector<int> prev(m,0);
+//         // first row to be copied exactly as it is
+//         for(int i =0; i<m ; i++) prev[i] = matrix[0][i];
+
+//         for(int i=1 ; i<n ; i++){
+//             vector<int> curr(m,0);
+//             for(int j=0 ; j<m ; j++){
+//                 int u = matrix[i][j]+prev[j];
+
+//                 int dl = matrix[i][j];
+//                 if(j-1 >= 0)  dl +=  prev[j-1];
+//                 else  dl = 1e9;
+
+//                 int dr = matrix[i][j];
+//                 if(j+1 < m) dr +=  prev[j+1];
+//                 else dr = 1e9; 
+
+//                 curr[j] = min(u, min(dl, dr));
+//             }
+//             prev = curr;
+//         }
+//         return *min_element(prev.begin(), prev.end());
+//     }
+// };
+
+
+
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int m = matrix[0].size();
         int n = matrix.size();
-        vector<int> prev(m,0);
-        // first row to be copied exactly as it is
-        for(int i =0; i<m ; i++) prev[i] = matrix[0][i];
+        int m = matrix[0].size();
 
-        for(int i=1 ; i<n ; i++){
-            vector<int> curr(m,0);
-            for(int j=0 ; j<m ; j++){
-                int u = matrix[i][j]+prev[j];
+        // dp[i][j] = minimum falling path sum to reach cell (i, j)
+        vector<vector<int>> dp(n, vector<int>(m, 0));
 
-                int dl = matrix[i][j];
-                if(j-1 >= 0)  dl +=  prev[j-1];
-                else  dl = 1e9;
-
-                int dr = matrix[i][j];
-                if(j+1 < m) dr +=  prev[j+1];
-                else dr = 1e9; 
-
-                curr[j] = min(u, min(dl, dr));
-            }
-            prev = curr;
+        // Base case: first row = same as matrix
+        for(int j = 0; j < m; j++) {
+            dp[0][j] = matrix[0][j];
         }
-        return *min_element(prev.begin(), prev.end());
+
+        // Fill DP from row 1 to row n-1
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                int up = matrix[i][j] + dp[i-1][j];
+
+                int leftDiag = 1e9;
+                if(j - 1 >= 0)
+                    leftDiag = matrix[i][j] + dp[i-1][j-1];
+
+                int rightDiag = 1e9;
+                if(j + 1 < m)
+                    rightDiag = matrix[i][j] + dp[i-1][j+1];
+
+                dp[i][j] = min({up, leftDiag, rightDiag});
+            }
+        }
+
+        // The answer is the minimum in the last row
+        int ans = INT_MAX;
+        for(int j = 0; j < m; j++)
+            ans = min(ans, dp[n-1][j]);
+
+        return ans;
     }
 };
