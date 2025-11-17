112044e6ce8abee5e13b5a89bfe70cc97261a5af
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-11-18 00:19:27 +0530

    Time: 18 ms (83.78%), Space: 13.8 MB (78.12%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -16,25 +16,49 @@ public:
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size();
         cuts.push_back(n);
-        cuts.insert(cuts.begin(),0); //go get the length of the stick
-        sort(cuts.begin(), cuts.end()); 
+        cuts.insert(cuts.begin(),0);
+        sort(cuts.begin(), cuts.end());
         vector<vector<int>> dp(c+2, vector<int>(c+2,0)); 
-
-        for(int i=c ; i>=1 ; i--)
-        {
-            for(int j=1 ; j<=c ; j++)
-            {
-                if(i > j) continue;
+        
+        for(int i=c ; i>=1 ; i--){
+            for(int j=1 ; j<=c ; j++){
+                if(i>j) continue;
                 int mini = INT_MAX;
-                // if(dp[i][j] != -1) return dp[i][j];
-                for(int ind=i ; ind <=j ; ind++){
-                    int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] +dp[ind+1][j];
-                    mini = min(mini, cost);
+
+                for(int ind = i ; ind<=j ; ind++){
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1]+ dp[ind+1][j];
+                    mini = min (mini, cost);
+                }
+                dp[i][j] = mini;
             }
-            dp[i][j] = mini;
-            }
+
         }
         return dp[1][c];
+
+
+
+
+        // int c = cuts.size();
+        // cuts.push_back(n);
+        // cuts.insert(cuts.begin(),0); //go get the length of the stick
+        // sort(cuts.begin(), cuts.end()); 
+        //  
+
+        // for(int i=c ; i>=1 ; i--)
+        // {
+        //     for(int j=1 ; j<=c ; j++)
+        //     {
+        //         if(i > j) continue;
+        //         int mini = INT_MAX;
+        //         // if(dp[i][j] != -1) return dp[i][j];
+        //         for(int ind=i ; ind <=j ; ind++){
+        //             int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] +dp[ind+1][j];
+        //             mini = min(mini, cost);
+        //     }
+        //     dp[i][j] = mini;
+        //     }
+        // }
+        // return dp[1][c];
 
     }
 };
