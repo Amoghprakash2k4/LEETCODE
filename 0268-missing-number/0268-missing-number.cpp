d71645e740ac39a3d3e528f64266ff0de148f184
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-12-15 00:50:31 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -11,15 +11,15 @@
 //     }
 // };
 
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int total = n * (n + 1) / 2;  // Expected sum of 0 to n
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        return total - sum;  // The missing number
-    }
-};
+        class Solution {
+        public:
+            int missingNumber(vector<int>& nums) {
+                int n = nums.size();
+                int total = n * (n + 1) / 2;  // Expected sum of 0 to n
+                int sum = 0;
+                for (int num : nums) {
+                    sum += num;
+                }
+                return total - sum;  // The missing number
+            }
+        };
