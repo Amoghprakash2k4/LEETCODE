7da085f4b03659dc7e6446d348829ad4544f1595
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2025-12-19 02:12:47 +0530

    Time: 19 ms (80.1%), Space: 13.9 MB (51.32%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -38,27 +38,5 @@ public:
 
 
 
-        // int c = cuts.size();
-        // cuts.push_back(n);
-        // cuts.insert(cuts.begin(),0); //go get the length of the stick
-        // sort(cuts.begin(), cuts.end()); 
-        //  
-
-        // for(int i=c ; i>=1 ; i--)
-        // {
-        //     for(int j=1 ; j<=c ; j++)
-        //     {
-        //         if(i > j) continue;
-        //         int mini = INT_MAX;
-        //         // if(dp[i][j] != -1) return dp[i][j];
-        //         for(int ind=i ; ind <=j ; ind++){
-        //             int cost = cuts[j+1] - cuts[i-1] + dp[i][ind-1] +dp[ind+1][j];
-        //             mini = min(mini, cost);
-        //     }
-        //     dp[i][j] = mini;
-        //     }
-        // }
-        // return dp[1][c];
-
     }
 };
