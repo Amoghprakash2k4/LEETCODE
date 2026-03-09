3234edda295f55759bd3116c2ae31ff534e9888e
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2026-03-10 00:44:01 +0530

    Time: 0 ms (100%), Space: 29.5 MB (67.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;  // Ensure k is within array bounds
+         k = k % n;  // Ensure k is within array bounds
 
         // Reverse entire array
         reverse(nums.begin(), nums.end());
