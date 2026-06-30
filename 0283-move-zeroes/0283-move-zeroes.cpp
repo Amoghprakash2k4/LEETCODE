33d1dc8767120622aca1c466cbca31d88e57f0a2
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2026-06-30 20:51:28 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-    //    int j=0; //place where 1st 0 is found
-    //    for(int i=0; i<nums.size() ; i++){
-    //         if(nums[i] != 0){
-    //             nums[j] = nums[i];
-    //             j++;
-    //         }
-    //    }
-    //    for(int i=j; i<nums.size();i++){
-    //         nums[i]=0;
-    //    }
-    // }
-
-
     int n= nums.size();
     int j=0 ;
     for(int i=0 ; i<n ; i++){
