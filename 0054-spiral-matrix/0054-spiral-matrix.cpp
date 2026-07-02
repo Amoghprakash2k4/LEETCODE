0b006f80acc49755395d79355883ec4c677ff7f5
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2026-07-02 19:52:25 +0530

    Time: 0 ms (100%), Space: 9.5 MB (15.63%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,43 +2,40 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> result;
-        if (matrix.empty() || matrix[0].empty()) return result;
-
         int top = 0;
-        int bottom = matrix.size() - 1;
+        int bottom = matrix.size()-1;
         int left = 0;
-        int right = matrix[0].size() - 1;
+        int right = matrix[0].size()-1;
 
-        while (top <= bottom && left <= right) {
-            // Move right
-            for (int col = left; col <= right; ++col) {
-                result.push_back(matrix[top][col]);
+        while(top <= bottom && left <= right){
+            for(int i=left ; i<=right ; i++){
+                result.push_back(matrix[top][i]);
+
             }
             top++;
 
-            // Move down
-            for (int row = top; row <= bottom; ++row) {
-                result.push_back(matrix[row][right]);
+            for(int i=top ; i<=bottom ; i++){
+                result.push_back(matrix[i][right]);
+
             }
             right--;
 
-            // Move left
-            if (top <= bottom) {
-                for (int col = right; col >= left; --col) {
-                    result.push_back(matrix[bottom][col]);
+            if(top <= bottom){
+                for(int i=right ; i>=left ; i--){
+                    result.push_back(matrix[bottom][i]);
                 }
-                bottom--;
             }
-
-            // Move up
-            if (left <= right) {
+            bottom--;
+    
+        if (left <= right) {
                 for (int row = bottom; row >= top; --row) {
                     result.push_back(matrix[row][left]);
                 }
                 left++;
             }
         }
-
         return result;
+
+
     }
 };
