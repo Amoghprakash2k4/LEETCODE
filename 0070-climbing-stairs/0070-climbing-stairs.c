fc3cedfee8406ad244f216374579b50bf5485bee
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2026-07-03 12:31:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -1,17 +1,15 @@
 int climbStairs(int n) {
-    if (n == 1) {
-        return 1;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+
+    int prev1 = 1;
+    int prev2 = 2;
+
+    for (int i = 3; i <= n; i++) {
+        int current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
     }
-    
-    int prev1 = 1;  // dp[i-1] (the number of ways to reach step i-1)
-    int prev2 = 1;  // dp[i-2] (the number of ways to reach step i-2)
-    int current;    // dp[i] (the number of ways to reach step i)
-    
-    for (int i = 2; i <= n; i++) {
-        current = prev1 + prev2;  // dp[i] = dp[i-1] + dp[i-2]
-        prev2 = prev1;            // Update dp[i-2] to dp[i-1]
-        prev1 = current;          // Update dp[i-1] to dp[i]
-    }
-    
-    return current;
+
+    return prev2;
 }
