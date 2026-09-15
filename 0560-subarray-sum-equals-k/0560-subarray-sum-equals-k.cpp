812b43b4c7313ca09025c0a1040a4c4a5f72329e
Amogh P <143121631+Amoghprakash2k4@users.noreply.github.com>
2026-09-15 14:53:21 +0530

    Time: 42 ms (59.26%), Space: 51 MB (18.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> map;
-        map[0] = 1;
-        int count = 0;
-        int sum = 0;
-        for(int i=0 ; i<nums.size() ; i++){
-            sum+=nums[i];
-            int remove = sum-k;
-            count += map[remove];
-            map[sum]+=1;
-        }
-        return count;
+    //     unordered_map<int, int> map;
+    //     int count =0 ;
+    //     map[0] = 1;
+    //     int sum = 0;
+
+    //     for(int i=0 ; i<nums.size() ; i++){
+    //         sum += nums[i];
+    //         int remove = sum - k;
+    //         count += map[remove];
+    //         map[sum]++;
+    //     }
+    //     return count;
+    // }
+
+    unordered_map<int,int> map;
+    int sum = 0;
+    int count = 0;
+    map[0] = 1;
+
+    for(int num : nums){
+        sum += num;
+        int remove = sum - k;
+        count += map[remove];
+        map[sum]++;
+    }
+    return count;
     }
 };
